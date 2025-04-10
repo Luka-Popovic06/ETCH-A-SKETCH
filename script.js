3cbbfd79d99671f8fb391e5aa17eb42dc67d3827
Luka Popovic <lukapopovic0111@gmail.com>
2025-04-10 15:08:06 +0200

    Creating variables

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
-"use strict";
+'use strict';
+const inputColor = document.querySelector('.inp-color');
+const eraser = document.querySelector('.btn-eraser');
+const clear = document.querySelector('.btn-clear');
+const cubes = document.querySelectorAll('.cube');
+const sketch = document.querySelector('#span-1');
+const selectColor = document.querySelector('.btn-mode');
+let color;

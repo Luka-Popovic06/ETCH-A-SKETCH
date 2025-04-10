e4c5442b864822f190d4befeae6a6d688921f5ec
Luka Popovic <lukapopovic0111@gmail.com>
2025-04-10 15:12:28 +0200

    Adding eventlistener

--- script.js
+++ script.js
@@ -6,3 +6,7 @@ const cubes = document.querySelectorAll('.cube');
 const sketch = document.querySelector('#span-1');
 const selectColor = document.querySelector('.btn-mode');
 let color;
+inputColor.addEventListener('input', function (e) {
+  color = e.target.value;
+  sketch.style.color = color;
+});
